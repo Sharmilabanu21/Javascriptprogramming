d47fb91951dfa123ec835fe487c525e52c25a9e8
Sharmilabanu21 <sharmimol21@gmail.com>
2025-09-01 16:40:23 +0530

    function.js added

---
+++ function.js
@@ -0,0 +1,10 @@
+var favactor="Vijai" //variable created
+var favactress="sai" //variable created
+var favmovie="ghilli" //variable created
+function favorite() //function created
+{ //block start
+    console.log("My favorite actor is "+favactor) //write in function
+    console.log("My favorite actress is "+favactress)//write in function
+    console.log("My favorite movie is "+favmovie)//write in function
+}
+favorite() //function call
