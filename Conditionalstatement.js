a6fa2caef132319146a40dd96e6fe0b158e612c5
Sharmilabanu21 <sharmimol21@gmail.com>
2025-09-02 15:25:53 +0530

    Conditionalstatement.js added

---
+++ Conditionalstatement.js
@@ -0,0 +1,26 @@
+//if the contional will be true... else the condition will be false
+//Example
+if(true) // when if condition is true if statement will be executed
+    console.log("Condition is true");
+else
+    console.log("Condition is false");    
+
+
+if(false) // when if condition is false esle statement will be executed
+    console.log("Condition is true");
+else
+    console.log("Condition is false");
+
+//__________________________________________________________________________//
+
+var rain=true //variable created
+if(rain) //condition is true
+    console.log("It's raining");
+else
+    console.log("Enjoy the sunshine");
+
+var rain=false //variable created
+if(rain) //condition is false
+    console.log("It's raining");
+else
+    console.log("Enjoy the sunshine");
