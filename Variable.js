616430d55d64753621cce539482a33eeea373a2c
Sharmilabanu21 <sharmimol21@gmail.com>
2025-09-04 20:28:04 +0530

    JavaScript

---
+++ Variable.js
@@ -0,0 +1,34 @@
+// Variables in js
+// var
+// let
+// const
+
+// var example
+var name = "Sharmila";
+console.log(name); // Sharmila
+
+var name = "Anshia"; // Redeclaration allowed
+console.log(name); // Anshia
+
+name = "Banu"; // Re-assignment allowed
+console.log(name); // Banu
+
+//___________________________________________________________//
+
+// let example
+let age = 22;
+console.log(age); // 22
+
+// let age = 23; //  Redeclaration not allowed
+age = 24; //  Re-assignment allowed
+console.log(age); // 24
+
+//___________________________________________________________//
+
+// const example
+const country = "India";
+console.log(country); // India
+
+// const country = "USA"; //  Redeclaration not allowed
+// country = "UK";        //  Re-assignment not allowed
+
